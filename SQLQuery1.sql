f813ca5c33728a219909397f4d89b62f68907670
Your Name <you@example.com>
2021-02-16 03:58:41 +0300

    Ödev 6.2

---
+++ SQLQuery1.sql
@@ -0,0 +1,7 @@
+﻿Select Products.ProductName 'Product Name',
+SUM([Order Details].UnitPrice*[Order Details].Quantity)'Total'
+FROM Products
+INNER JOIN[Order Details]
+ON Products.ProductID=[Order Details].ProductID
+INNER JOIN Orders ON Orders.OrderID=[Order Details].OrderID
+GROUP BY Products.ProductName
